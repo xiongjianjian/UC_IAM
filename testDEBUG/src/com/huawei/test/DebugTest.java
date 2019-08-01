71142297257cb43630ab2416168ca9af2f38803a
Administrator <1784790610@qq.com>
2019-08-01 19:34:46 +0800

    testDebug

---
+++ testDEBUG/src/com/huawei/test/DebugTest.java
@@ -0,0 +1,21 @@
+package com.huawei.test;
+
+import java.util.HashMap;
+
+/**
+ * @author xiongjianjian
+ * @dte 2019/8/1 - 18:14
+ */
+public class DebugTest {
+    public static void main(String[] args) {
+        HashMap<String,String> map=new HashMap<>();
+        map.put("name","Tom");
+        map.put("age","12");
+        map.put("school","Tsinghua");
+        map.put("major","computer");
+        String age = map.get("age");
+        System.out.println("age = " + age);
+        map.remove("major");
+        System.out.println(map);
+    }
+}
